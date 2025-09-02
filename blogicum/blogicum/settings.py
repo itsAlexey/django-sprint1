3b23f1dea936783b0cf8ff9ddbc82162d7872a93
Alexey Romachev <aaromachev1990@gmail.com>
2025-09-02 18:19:13 +0300

    Flake8

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -1,5 +1,3 @@
-import os
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +21,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'pages.apps.PagesConfig',
-    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
